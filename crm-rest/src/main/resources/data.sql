340f7f285e895496dec0bdeab3f26576c41b413e
RAJAT GAURAV <53166907+rajatgaurav@users.noreply.github.com>
2022-09-26 13:03:54 +0530

    Add files via upload

---
+++ crm-rest/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (id, name, birth_date, email) VALUES (10001, 'rajat', sysdate(), 'admin@rajatgaurav.net');
+INSERT INTO customer (id, name, birth_date, email) VALUES (10002, 'aman', sysdate(), 'aman@rajatgaurav.net');
+INSERT INTO customer (id, name, birth_date, email) VALUES (10003, 'gaurav', sysdate(), 'gaurav@rajatgaurav.net');
